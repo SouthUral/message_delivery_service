c2b9bec1d9a11e08127850cc982526e3caf44e64
vladimir <vladimir1012kov@gmail.com>
2023-03-10 20:50:44 +0300

    сервис можифицирован, добавлена обратотка ошибок

--- mt.py
+++ mt.py
@@ -1,25 +1,23 @@
 import asyncio
 import aioamqp
+from asyncpg.exceptions import InterfaceError
 import asyncpg
 import json
 from loguru import logger
+import dataclasses
 
 
-def url_rb():
-    port = "5672"
-    user = "guest"
-    password = "guest"
-    v_host = "new"
-    host = "localhost"
-    return f"amqp://{user}:{password}@{host}:{port}/"
+@dataclasses.dataclass
+class url_key:
+    port: str = "5432"
+    user: str = "user"
+    password: str = "password"
+    host: str = "localhost"
+    spec_param: str = "postgres"
+    connection: str = "postgresql"
 
-def url_db():
-    port = "54320"
-    user = "admin"
-    password = "1234567"
-    host = "127.0.0.1"
-    database = "db01"
-    return f"postgresql://{user}:{password}@{host}:{port}/{database}"
+    def __call__(self) -> str: 
+        return f"{self.connection}://{self.user}:{self.password}@{self.host}:{self.port}/{self.spec_param}"
 
 
 class MessageTransmitter:
@@ -33,15 +31,37 @@ class MessageTransmitter:
         self.bd_conn = None
 
     async def _connect_bd(self):
-        self.bd_conn = await asyncpg.connect(self.url_db)
-        logger.success(f"Connected to PostgresQL")
+        try:
+            self.bd_conn = await asyncpg.connect(self.url_db)
+            logger.success(f"Connected to PostgresQL")
+        except OSError as err:
+            logger.error(f"connect_bd error: {err}")
+            asyncio.get_event_loop().stop()
 
     async def _connect_rb(self):
         try:
-            self.transport, self.protocol = await aioamqp.from_url(url=self.url_rabbit)
+            url = aioamqp.urlparse(url=self.url_rabbit)
+            self.transport, self.protocol = await aioamqp.connect(
+                host=url.hostname,
+                port=url.port,
+                login=url.username,
+                password=url.password,
+                on_error=self.error_callback,
+                client_properties={'heartbeat': 10,
+                                   'program_name': 'some'}
+            )
+            self.channel = await self.protocol.channel()
             logger.success("RabbitMQ connected!")
-        except aioamqp.AmqpClosedConnection as _err:
-            logger.error(f"RabbitMQ connection error: {_err}")
+        except aioamqp.AmqpClosedConnection as err:
+            logger.error(f"RabbitMQ connection error: {err}")
+        except OSError as err:
+            logger.error(f"RabbitMQ connection error: {err}")
+            asyncio.get_event_loop().stop()
+
+    async def error_callback(self, exception):
+        logger.error(f"RabbitMQ error: {exception}")
+        asyncio.get_event_loop().stop()
+
 
     async def _check_offset(self):
         last_value_db = await self.bd_conn.fetchrow('''select 
@@ -54,25 +74,30 @@ class MessageTransmitter:
             self.offset = last_value_db['offset_number'] + 1
 
     async def _events_stream(self):
-        self.channel = await self.protocol.channel()
-        await self._check_offset()
-        await self.channel.basic_qos(prefetch_count=100)
-        await self.channel.basic_consume(
-            callback=self._callback,
-            queue_name="new",
-            arguments={
-                "x-stream-offset": self.offset
-            })
-        await asyncio.sleep(1)
-        logger.success("Сообщения прочитаны")
+        try:
+            await self._check_offset()
+            await self.channel.basic_qos(prefetch_count=1)
+            await self.channel.basic_consume(
+                callback=self._callback,
+                queue_name="new",
+                arguments={
+                    "x-stream-offset": self.offset
+                },)
+            logger.success("Сообщения прочитаны")
+        except:
+            logger.error(f"events_stream error")
 
     async def _callback(self, channel, body, envelope, properties):
         offset = properties.headers["x-stream-offset"]
         message = json.dumps({'message': body.decode(), 'offset': int(offset)})
-        await self.bd_conn.execute('''call sh_signal.calc_quality($1)''', message)
-        logger.success(f"The message has been sent")
-        print(" [x] Received {} - {}".format(body, offset))
+        logger.success(f"messages received offset:{offset}")
         await channel.basic_client_ack(delivery_tag=envelope.delivery_tag)
+        try:
+            await self.bd_conn.execute('''call sh_signal.calc_quality($1)''', message)
+            logger.success("messages are sent to the database")
+        except InterfaceError as err:
+            logger.error(f"error callback 2: {err}")
+            asyncio.get_event_loop().stop()
 
     async def _close(self):
         await self.protocol.close()
@@ -82,14 +107,16 @@ class MessageTransmitter:
         logger.success(f"Close connect to PostgresQL")
 
     async def __call__(self, *args, **kwds):
-        await self._connect_bd()
-        await self._connect_rb()
+        await asyncio.gather(
+            self._connect_bd(),
+            self._connect_rb()
+            )
         await self._events_stream()
 
-
-mt = MessageTransmitter(url_rabbit=url_rb(), url_db=url_db())
-mt()
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(mt())
-loop.run_forever()
+if __name__ == "__main__":
+    url_rb = url_key(port="5672", user="guest", password="guest", connection="amqp", spec_param="")
+    url_db = url_key(port="54320", user="admin", password="1234567", spec_param="db01")
+    mt = MessageTransmitter(url_rabbit=url_rb(), url_db=url_db())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(mt())
+    loop.run_forever()
